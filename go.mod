f168381ed2877f0716874c0d59605c128e4c1c47
Ibragim <yandaroviba@mail.ru>
2025-11-17 21:07:46 +0300

    set up coordinator structure and add parser

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/avraam311/distributed-gogrep
 
 go 1.25.3
+
+require github.com/spf13/pflag v1.0.10
---
+++ internal/coordinator/parser.go
@@ -0,0 +1,77 @@
+package coordinator
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/spf13/pflag"
+)
+
+type Parser struct {
+	FlagA    int
+	FlagB    int
+	FlagC    int
+	FlagCc   bool
+	FlagI    bool
+	FlagV    bool
+	FlagF    bool
+	FlagN    bool
+	Template string
+	Filename string
+	Strings  []string
+}
+
+func NewParser() *Parser {
+	flagA := pflag.IntP("A", "A", 0, "print strings after")
+	flagB := pflag.IntP("B", "B", 0, "print string before")
+	flagC := pflag.IntP("C", "C", 0, "print string after and before")
+	flagCc := pflag.BoolP("c", "c", false, "print number of matches")
+	flagI := pflag.BoolP("i", "i", false, "ignore registre")
+	flagV := pflag.BoolP("v", "v", false, "print strings that don't match")
+	flagF := pflag.BoolP("F", "F", false, "consider template as string")
+	flagN := pflag.BoolP("n", "n", false, "print number of string before each string")
+	pflag.Parse()
+	fileName := pflag.Arg(1)
+	template := pflag.Arg(0)
+
+	strs := getRows(fileName)
+	flags := Parser{
+		FlagA:    *flagA,
+		FlagB:    *flagB,
+		FlagC:    *flagC,
+		FlagCc:   *flagCc,
+		FlagI:    *flagI,
+		FlagV:    *flagV,
+		FlagF:    *flagF,
+		FlagN:    *flagN,
+		Template: template,
+		Filename: fileName,
+		Strings:  strs,
+	}
+
+	return &flags
+}
+
+func getRows(fileName string) []string {
+	var data []byte
+	var err error
+
+	if fileName == "" {
+		data, err = io.ReadAll(os.Stdin)
+		if err != nil {
+			fmt.Println("error reading from stdin:", err)
+			os.Exit(1)
+		}
+	} else {
+		data, err = os.ReadFile(fileName)
+		if err != nil {
+			fmt.Println("error reading from file:", err)
+			os.Exit(1)
+		}
+	}
+
+	rows := strings.Split(strings.ReplaceAll(string(data), "\r\n", "\n"), "\n")
+	return rows
+}
