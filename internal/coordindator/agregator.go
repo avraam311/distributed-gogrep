aae425b90dd3ce985f3ac3eaa05604993ca93a1a
Ibragim <yandaroviba@mail.ru>
2025-11-17 22:09:07 +0300

    add agregator

--- internal/coordindator/agregator.go
+++ internal/coordindator/agregator.go
@@ -1,1 +1,24 @@
 package coordinator
+
+import "fmt"
+
+type Agregator struct {
+	matches [][]string
+}
+
+func NewAgregator(matches [][]string) *Agregator {
+	return &Agregator{}
+}
+
+func (a *Agregator) PrintResultWithNumbers() {
+	for _, pair := range a.matches {
+		fmt.Print(pair[0])
+		fmt.Println(pair[1])
+	}
+}
+
+func (a *Agregator) PrintResult() {
+	for _, match := range a.matches {
+		fmt.Println(match[1])
+	}
+}
